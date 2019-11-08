da41a2c40c45593ff5278b97cde00dee09d8f3ec
angel-zepeda <angel.zepeda0508@hotmail.com>
2019-11-08 13:26:52 -0600

    Change route for delete files

--- controllers/files.js
+++ controllers/files.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 const deleteFiles = (req, res) => {
     const { name } = req.params;
-    fs.unlinkSync('157.245.12.180:5000/' + name);
+    fs.unlinkSync('/root/Issste/isssteCGapi/files' + name);
 }
 
 module.exports = {
